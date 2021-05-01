f6a3f1b8252af535902b7c4744a0c66a9cde5a57
Frederik Collignon <frederik.collignon@minfin.fed.be>
2021-05-01 13:46:30 +0200

    Adder @GetMapping to OrderController to test

--- src/main/java/be/frederikcollignon/order/api/OrderController.java
+++ src/main/java/be/frederikcollignon/order/api/OrderController.java
@@ -1,4 +1,15 @@
 package be.frederikcollignon.order.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/orders", produces = "application/json")
 public class OrderController {
+
+    @GetMapping(produces = "application/json")
+    public String returnString() {
+        return "Hello World!";
+    }
 }
