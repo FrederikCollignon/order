4c5dfb760ff6b3792d32101ad9b92a42e810b8ac
Frederik Collignon <frederik.collignon@minfin.fed.be>
2021-05-01 15:11:00 +0200

    Added OrderMapper

---
+++ src/main/java/be/frederikcollignon/order/service/mapper/OrderMapper.java
@@ -0,0 +1,23 @@
+package be.frederikcollignon.order.service.mapper;
+
+import be.frederikcollignon.order.domain.entity.Order;
+import be.frederikcollignon.order.service.dto.request.CreateOrderDTO;
+import be.frederikcollignon.order.service.dto.response.OrderDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderMapper {
+
+    public Order fromDto(CreateOrderDTO createOrderDTO) {
+        return new Order(
+                createOrderDTO.getTestName()
+        );
+    }
+
+    public OrderDTO toDto(Order order) {
+        return new OrderDTO()
+                //.setId(order.getId())
+                .setName(order.getName());
+    }
+
+}
