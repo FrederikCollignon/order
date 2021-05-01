abc60bab85613a5f125548d38aba8c66de8b6ab0
Frederik Collignon <frederik.collignon@minfin.fed.be>
2021-05-01 15:11:20 +0200

    Added OrderDTO

---
+++ src/main/java/be/frederikcollignon/order/service/dto/response/OrderDTO.java
@@ -0,0 +1,33 @@
+package be.frederikcollignon.order.service.dto.response;
+
+public class OrderDTO {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public OrderDTO setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public OrderDTO setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDTO{" +
+                "id=" + //id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
