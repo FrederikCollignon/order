a97a0316215c96a3da46b1eb0511b53b8fa8a6b9
Frederik Collignon <frederik.collignon@minfin.fed.be>
2021-05-06 09:28:30 +0200

    Stand van zaken na Tim zijn uitleg story 3, nu ga ik terug naar de laatste commit van story 2 en probeer story 3 van scratch

---
+++ src/main/java/be/frederikcollignon/order/service/dto/request/CreateItemGroupDTO.java
@@ -0,0 +1,15 @@
+package be.frederikcollignon.order.service.dto.request;
+
+public class CreateItemGroupDTO {
+
+    private int itemId;
+    private int amount;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
